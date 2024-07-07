d2ff6ac8bda7dae1aa8cb32df5276b442bb3c3a7
Anand Satheesh <anands282@gmail.com>
2024-07-07 18:11:21 +0530

    updated creators class

--- creational_patterns/factory/creators.py
+++ creational_patterns/factory/creators.py
@@ -1,6 +1,13 @@
+from creational_patterns.factory.button import Button
+from html_button import HtmlButton
+from windows_button import WindowsButton
 from dialog import Dialog
 
 class WindowsDialog(Dialog):
+    def create_button(self) -> Button:
+        return WindowsButton()
 
 
 class WebDialog(Dialog):
+    def create_button(self) -> Button:
+        return HtmlButton()
